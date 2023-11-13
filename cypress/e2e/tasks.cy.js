fc2b58b056e8be1ff4d4e20e6f3b4c522483e9b2
Amirhossein <amirhossinbalagar@gmail.com>
2023-11-13 14:17:27 +0330

    repeat test with cancel button and make usable fn

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,9 +1,22 @@
+function openAndCheck(command) {
+  cy.contains("Add Task").click();
+  command();
+  cy.get(".backdrop").should("not.exist");
+  cy.get(".modal").should("not.exist");
+}
+
 describe("tasks managment", () => {
   it("should open and close the new task modal", () => {
     cy.visit("http://localhost:5173/");
-    cy.contains("Add Task").click();
-    cy.get(".backdrop").click({
-      force: true,
+
+    openAndCheck(() => {
+      cy.get(".backdrop").click({
+        force: true,
+      });
+    });
+
+    openAndCheck(() => {
+      cy.contains("Cancel").click();
     });
   });
 });
