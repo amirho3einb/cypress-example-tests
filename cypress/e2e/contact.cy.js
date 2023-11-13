136fd3624f61d1f8e538488e4022041634571ed6
Amirhossein <amirhossinbalagar@gmail.com>
2023-11-13 17:36:21 +0330

    Chaining Commands

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -4,10 +4,10 @@ describe("contact form", () => {
     cy.get('[data-cy = "contact-input-name"]').type("Sama");
     cy.get('[data-cy = "contact-input-message"]').type("I love You");
     cy.get('[data-cy = "contact-input-email"]').type("sama@gmail.com");
-    cy.get("form button").contains("Send Message");
-    cy.get("form button").should("not.have.attr", "disabled");
+    cy.get("form button")
+      .contains("Send Message")
+      .and("not.have.attr", "disabled");
     cy.get("form button").click();
-    cy.get("form button").contains("Sending");
-    cy.get("form button").should("have.attr", "disabled");
+    cy.get("form button").contains("Sending").should("have.attr", "disabled");
   });
 });
