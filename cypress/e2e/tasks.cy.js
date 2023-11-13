4727f89dda54e4961bf39bc9c3af1f810c8f88fa
Amirhossein <amirhossinbalagar@gmail.com>
2023-11-13 14:06:39 +0330

    Simulating User Interaction

---
+++ cypress/e2e/tasks.cy.js
@@ -0,0 +1,9 @@
+describe("tasks managment", () => {
+  it("should open and close the new task modal", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("Add Task").click();
+    cy.get(".backdrop").click({
+      force: true,
+    });
+  });
+});
