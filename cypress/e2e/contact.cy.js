b6204317da6b8fe7efe248694f61c5128d897ef9
Amirhossein <amirhossinbalagar@gmail.com>
2023-11-14 16:50:55 +0330

    Configuring the baseURL

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -1,6 +1,6 @@
 describe("contact form", () => {
   it("should submit the form", () => {
-    cy.visit("http://localhost:5173/about");
+    cy.visit("/about");
     cy.get('[data-cy = "contact-input-name"]').type("Sama");
     cy.get('[data-cy = "contact-input-message"]').type("I love You");
     // cy.get('[data-cy = "contact-input-email"]').type("sama@gmail.com");
@@ -19,7 +19,7 @@ describe("contact form", () => {
   });
 
   it("should validate the form input", () => {
-    cy.visit("http://localhost:5173/about");
+    cy.visit("/about");
     cy.get('[data-cy = "contact-btn-submit"]').as("submitBtn");
     cy.get("@submitBtn").click();
     cy.get("@submitBtn").then((el) => {
--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,6 +1,6 @@
 describe("page navigation", () => {
   it("should navigate beetween pages", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("/");
     // cy.get("header a").last();
     cy.get('[data-cy = "header-about-link"]').click();
     // cy.get("h1").contains("About Us").should("have.length", 1);
