92d23238c3362252138b8f3f33d5c3a9fbeaae17
Amirhossein <amirhossinbalagar@gmail.com>
2023-11-15 11:53:03 +0330

    WorkinwithStubsSpies,Fixtures&ManipulatingTheClock

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,10 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173",
+    baseUrl: 'http://localhost:5173',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on("task", {
-        seedDatabase(filename) {
-          // Run your Nodejs code
-          // e.g edit a file here
-          return filename;
-        },
-      });
     },
   },
 });
