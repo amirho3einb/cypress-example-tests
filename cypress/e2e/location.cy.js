87c82c67e1e5f7a4779ecca46b490ce7068d2841
Amirhossein <amirhossinbalagar@gmail.com>
2023-11-15 14:39:44 +0330

    Using Stubs Across Tests

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -1,15 +1,31 @@
 /// <reference types="cypress" />
 
 describe("share location", () => {
-  it("should fetch the user location", () => {
+  beforeEach(() => {
     cy.visit("/").then((win) => {
-      cy.stub(win.navigator.geolocation, "getCurrentPosition").as(
-        "getUserPosition"
-      );
+      cy.stub(win.navigator.geolocation, "getCurrentPosition")
+        .as("getUserPosition")
+        .callsFake((cb) => {
+          setTimeout(() => {
+            cb({
+              coords: {
+                latitude: 37.5,
+                longitude: 48.01,
+              },
+            });
+          }, 100);
+        });
     });
+  });
+  it("should fetch the user location", () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get("@getUserPosition").should("have.been.called");
     cy.get('[data-cy="get-loc-btn"]').should("be.disabled");
     cy.get('[data-cy="actions"]').should("contain", "Location fetched");
+  });
+
+  it("should share a location URL", () => {
+    cy.get('[data-cy="name-input"]').type("Amirhossein");
+    cy.get('[data-cy="get-loc-btn"]').click();
   });
 });
