23102b5daed04238c19056aaea0d978f17e3ffc3
Amirhossein <amirhossinbalagar@gmail.com>
2023-11-15 12:13:37 +0330

    Creating & Using a Stub

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -1,8 +1,15 @@
 /// <reference types="cypress" />
 
-describe('share location', () => {
-  it('should fetch the user location', () => {
-    cy.visit('/');
+describe("share location", () => {
+  it("should fetch the user location", () => {
+    cy.visit("/").then((win) => {
+      cy.stub(win.navigator.geolocation, "getCurrentPosition").as(
+        "getUserPosition"
+      );
+    });
     cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get("@getUserPosition").should("have.been.called");
+    cy.get('[data-cy="get-loc-btn"]').should("be.disabled");
+    cy.get('[data-cy="actions"]').should("contain", "Location fetched");
   });
 });
