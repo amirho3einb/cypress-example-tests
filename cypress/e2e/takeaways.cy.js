2cab86e28f22ed2f47aca8828fd1bd083d3a438f
Amirhossein <amirhossinbalagar@gmail.com>
2023-11-26 17:28:34 +0330

    update

--- cypress/e2e/takeaways.cy.js
+++ cypress/e2e/takeaways.cy.js
@@ -8,4 +8,20 @@ describe("Takeaways", () => {
     cy.visit("/");
     cy.get('[data-cy="takeaway-item"]').should("have.length", 2);
   });
+
+  it("should add a new takeaway", () => {
+    cy.intercept("POST", "/takeaways/new*", "success").as("createTakeaway");
+    cy.login();
+    cy.visit("/takeaways/new");
+    cy.get('[data-cy="title"]').click();
+    cy.get('[data-cy="title"]').should("not.be.disabled");
+    cy.get('[data-cy="title"]').type("testTitle1");
+    cy.get('[data-cy="body"]').click();
+    cy.get('[data-cy="body"]').should("not.be.disabled");
+    cy.get('[data-cy="body"]').type("testBody1");
+    cy.get('[data-cy="create-takeaway"]').click();
+    cy.wait("@createTakeaway")
+      .its("request.body")
+      .should("match", /testTitle1.*testBody1/);
+  });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -36,16 +36,28 @@
 //   }
 // }
 
+Cypress.Commands.add("login", () => {
+  cy.visit("/login");
+  cy.get('[data-cy="auth-email"]').click();
+  cy.get('[data-cy="auth-email"]').should("not.be.disabled");
+  cy.get('[data-cy="auth-email"]').type("amirbalgar1377@example.com");
+  cy.get('[data-cy="auth-password"]').click();
+  cy.get('[data-cy="auth-password"]').should("not.be.disabled");
+  cy.get('[data-cy="auth-password"]').type("42153466");
+  cy.get('[data-cy="auth-submit"]').click();
+  cy.location("pathname").should("eq", "/takeaways");
+  cy.getCookie("__session").its("value").should("not.be.empty");
+});
 
 // the below code snippet is required to handle a React hydration bug that would cause tests to fail
 // it's only a workaround until this React behavior / bug is fixed
-Cypress.on('uncaught:exception', (err) => {
+Cypress.on("uncaught:exception", (err) => {
   // we check if the error is
   if (
-    err.message.includes('Minified React error #418;') ||
-    err.message.includes('Minified React error #423;') ||
-    err.message.includes('hydrating') ||
-    err.message.includes('Hydration')
+    err.message.includes("Minified React error #418;") ||
+    err.message.includes("Minified React error #423;") ||
+    err.message.includes("hydrating") ||
+    err.message.includes("Hydration")
   ) {
     return false;
   }
